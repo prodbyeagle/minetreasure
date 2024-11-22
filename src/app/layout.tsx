42b6dab3417f273a385ab9adb4e1b2eb6d8bf26c
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-22 20:57:20 +0100

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mine Treasure",
+  description: "Mine treasure is a Minecraft datapack which aims to enhance the mining experience. This datapack does so by making treasure randomly spawn while mining.",
+  icons: {
+    icon: "/images/Mine_Treasure.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,12 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
+      <body className={`antialiased`}>
         {children}
-
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
